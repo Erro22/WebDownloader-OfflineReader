23735fdef1763fcbdc953c73d76bd6ead5d7db8a
Andrew <koolandrej310@gmail.com>
2026-05-08 17:44:26 +0300

    Add version verification Toast to onCreate

--- app/src/main/java/com/example/webdownloader/MainActivity.kt
+++ app/src/main/java/com/example/webdownloader/MainActivity.kt
@@ -70,6 +70,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("WebDownloader", "!!! DEBUG VERSION LOADED !!!")
+        Toast.makeText(this, "Debug Version Loaded", Toast.LENGTH_LONG).show()
         setContentView(R.layout.activity_main)
 
         initViews()
